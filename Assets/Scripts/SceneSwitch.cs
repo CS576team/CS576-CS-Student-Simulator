acb17cd441fa333c99a29f54222008c305d41efb
rxiong19 <rxiong@umass.edu>
2022-12-04 14:20:35 -0500

    open scene created

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public string input;
+    
+    public void switchScene(string scenename)
+    {
+        Debug.Log(scenename);
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void readInput(string i){
+        input = i;
+        Debug.Log(input);
+        if (input == "Start" || input == "start"){
+            SceneManager.LoadScene("Dorm");
+        }
+    }
+
+
+}
