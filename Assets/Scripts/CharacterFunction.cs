31e3b1dd0472686ab18183ae883d99b32e3c4e82
rxiong19 <rxiong@umass.edu>
2022-12-09 00:27:32 -0500

    ad confirmation box before leaving dorm scene

--- Assets/Scripts/CharacterFunction.cs
+++ Assets/Scripts/CharacterFunction.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class CharacterFunction : MonoBehaviour
 {
+
+    public GameObject yes;
+    public GameObject no;
+    public string msg;
+    public TMP_Text confirm_text;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +27,16 @@ public class CharacterFunction : MonoBehaviour
     private void OnCollisionEnter(Collision other) {
         Debug.Log(other.gameObject.name);
         if (other.gameObject.name == "OfficeChair"){
-            SceneManager.LoadScene("Computer");
+            msg = "Computer";
+            confirm_text.text = "Start Studying";
+            yes.SetActive(true);
+            no.SetActive(true);
+        }
+        else if (other.gameObject.name == "Door"){
+            msg = "Classroom";
+            confirm_text.text = "Leave Dorm";
+            yes.SetActive(true);
+            no.SetActive(true);
         }
     }
 }
